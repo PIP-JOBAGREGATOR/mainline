6d8e671b544b5d6984006e9dc4f2e900bebd5e18
auras3590 <auras3590@yahoo.com>
2012-04-29 18:16:45 +0300

    modificat introducerea cu duplicate

--- core/pipeline/scrappers/database_handler.py
+++ core/pipeline/scrappers/database_handler.py
@@ -1,19 +1,38 @@
 import MySQLdb
-
+import simplejson as json
 
 class DatabaseHandler:
     def __init__(self):
         self.conn = MySQLdb.connect("localhost","root","root","job_agregator")
-	
+
+    def GetJson(self,job):
+        dictionar = job.__dict
+        dummy = dictionar.pop("id")
+        dummy = dictionar.pop("hash")
+        retval = json.dumps(dictionar)
+        return retval
+    
     def Read(self,timestamp):
         rows = []
         cursor = self.conn.cursor();
         data = cursor.execute("select html from jobs_html where date >= %s", [timestamp])
         for html_name in cursor.fetchall():
             rows.append(html_name)
-        return rows
+        return rows    
 
-    def Write(self,title,description,salary,company):
-        params = [title, description, salary, company]
+    def Write(self,job):
+        job_json = self.GetJson(job)
+        hashString = job.hash
+        job_id = job.id
         cursor = self.conn.cursor()
-        cursor.execute("insert into jobs(title,description,salary,company) values(%s, %s, %s, %s)", params)
+        data = cursor.execute("select COUNT(*) from jobs where jobid = %s",job_id)
+        size = 0;
+        for count in cursor.fetchall():
+            ++size
+        if(size == 0):
+            cursor.execute("insert into jobs(jobid,jobinfo,hash) values(%s, %s, %s)",job_id, job_json, hashString)
+        else:
+            cursor.execute("UPDATE jobs SET jobinfo = %s, hash = %s WHERE jobid = %s", job_json, hashString, job_id)
+            
+
+    
