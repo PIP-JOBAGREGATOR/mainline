a243d11532217f3ab7fa03e0f76910337c9ca34d
auras3590 <auras3590@yahoo.com>
2012-04-29 20:07:00 +0300

    hash processing implementation

--- core/pipeline/scrappers/scrapper_base.py
+++ core/pipeline/scrappers/scrapper_base.py
@@ -1,5 +1,6 @@
 from BeautifulSoup import BeautifulSoup
 import urllib
+import md5
 from database_handler import DatabaseHandler
 
 class Object:
@@ -8,7 +9,16 @@ class Object:
 class ScrapperBase:
     def __init__(self,timestamp):
         self.database = DatabaseHandler()
-        self.timestamp = timestamp 
+        self.timestamp = timestamp
+
+    def processHash(self,processed):
+        string = ''
+        string += self.job.job_title
+        string += self.job.employer
+        hasher = md5.new()
+        hasher.update(string)
+        hashed = hasher.hexdigest()        
+        processed.hash = hashed
 
     def run(self):
         items = self.read()
@@ -16,12 +26,13 @@ class ScrapperBase:
         for item in items:
             try:
                 processed_item = self.process_item(item)
+                self.processHash(processed_item)
                 self.write(processed_item)
             except:
                 pass
 
     def write(self,job):
-        self.database.Write(job.job_title,job.description,job.salary,job.employer)
+        self.database.Write(job)
 
     def read(self):
         rows = self.database.Read(self.timestamp);
