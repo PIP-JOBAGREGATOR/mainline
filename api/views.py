1995d0a2de61e2674797ef7307cb42e5f7b630b5
Tiberiu Savin <tiberiu.savin@gmail.com>
2012-05-19 20:37:39 +0300

    Fixed damn server

--- api/views.py
+++ api/views.py
@@ -9,34 +9,64 @@ from django.http import HttpResponse
 
 RESULTS_PER_PAGE = 10
 
+def get_positions(cv):
+    positions = cv["positions"]["values"]
+    return [pos["title"] for pos in positions]
+
+def get_education_jobs(cv):
+    educations = cv["educations"]["values"]
+    results = []
+    for edu in educations:
+        school_name = edu["schoolName"].lower()
+        common_bits = {}
+        for key in COLLEGE_JOB:
+            school_bits = set(school_name.split(" "))
+            college_bits = set(key.lower().split(" "))
+            common_bits[key] = school_bits & college_bits
+
+        for key in common_bits:
+            if len(common_bits) > 2:
+                results.extend(COLLEGE_JOB[key])
+
+    return results
+
+def parse_cv(cv):
+    jobs = get_positions(cv)
+    jobs.extend(get_education_jobs(cv))
+    return jobs
+
 def search(request):
-    response = HttpResponse()
+    try:
+        response = HttpResponse()
 
-    if request.method == 'POST':
-        content = request.POST.get('content')
-        data = json.loads(content)
+        if request.method == 'POST':
+            content = request.POST.get('content')
+            data = json.loads(content)
 
-        offset = data.get("offset", 0)
-        size = data.get("size", RESULTS_PER_PAGE)
-        queryString = data["queryString"]
-        cv = data.get("cv", "")
+            offset = data.get("offset", 0)
+            size = data.get("size", RESULTS_PER_PAGE)
+            queryString = data["queryString"]
+            cv = data.get("cv", "")
+            queryString += " " + ' '.join(parse_cv(cv))    
 
-        index = IndexManager()
+            index = IndexManager()
 
-        temp = index.query(queryString)
-        results = []
-        for job in temp:
-            result = {}
-            result["titlu"] = job['title']
-            result["descriere"] = job['description'][:200]
-            result["link"] = job['url']
-            results.append(result)      
+            temp = index.query(queryString)
+            results = []
+            for job in temp:
+                result = {}
+                result["titlu"] = job['title']
+                result["descriere"] = job['description'][:200]
+                result["link"] = job['url']
+                results.append(result)      
 
-        response.content = json.dumps(results)
-        response.status_code = 200
-        index.close()
-    else:
-        response.status_code = 400
+            response.content = json.dumps(results)
+            response.status_code = 200
+            index.close()
+        else:
+            response.status_code = 400
+    except:
+        import traceback
+        traceback.print_exc()
 
-    return response;
-
+    return response
