35f6d8a1385ce283ed162d36fd1022157c2aaf04
Robert Hasna <hasna.robert@gmail.com>
2012-05-25 22:21:25 +0300

    fixed some flows in the workflow

--- ui/app.js
+++ ui/app.js
@@ -1,19 +1,19 @@
 "use strict";
 
-var app = (function() {	
+var app = (function() {
 	var hostname = window.location.hostname;
 	if (hostname.length == 0) {
 		hostname = "https://192.168.1.105:8443";
 	}
 	var apiURL = hostname + "/api/";
-	
+
 	var pageAPI = null;
-	
+
 	var bootstrap = function() {
 		Environment.collectInfo();
 		LayoutManager.clean();
 		pageAPI = LayoutManager.buildUI();
-		
+
 		var buildQueryJSON = function(data, offset) {
 			return JSON.stringify({
 				"offset": offset,
@@ -22,50 +22,37 @@ var app = (function() {
 				"cv": data.cv
 			});
 		};
-	        var saveCV = function(){
-	                var jsonOb = pageAPI.cvInputAPI.saveCV();
-       	        	var json = buildQueryJSON(jsonOb, 0);
-                	$.ajax({
-                	"url": window.hostname + "/api/cv/set/",
-                	"async": true,
-                	"type": "post",
-                	"data": {"content": json},
-                	"success": function (data, textStatus, jqXHR) {
-                        	window.console.log("OK");
-                	},
-                	"error": function(jqXHR, textStatus, errorThrown) {
-                        	window.console.log("A crapat : " + errorThrown);
-                		}
-                	});
-        	}	
 
-        	pageAPI.cvInputAPI.addSaveCVCallback(saveCV);
+        var saveCV = function(){
+	      var jsonOb = pageAPI.cvInputAPI.saveCV();
+	      var json = buildQueryJSON(jsonOb, 0);
+	      $.ajax({
+	        "url": window.hostname + "/api/cv/set/",
+	        "async": true,
+	        "type": "post",
+	        "data": {"content": json},
+	        "success": function (data, textStatus, jqXHR) {
+              window.console.log("Cv salvat cu succes!");
+            },
+	        "error": function(jqXHR, textStatus, errorThrown) {
+	          window.console.log("A crapat : " + errorThrown);
+		    }
+	      });
+	    }
 
-		
+	  pageAPI.cvInputAPI.addSaveCVCallback(saveCV);
+
 		var searchCallback = function() {
-			var jsonOb = pageAPI.cvInputAPI.getData();
+            var jsonOb = pageAPI.cvInputAPI.getData();
 			var json = buildQueryJSON(jsonOb, 0);
-			window.console.log("ASTAAAA" + json);
-			
-			$.ajax({
-				/*"url": (function(){
-					if (jsonOb.cv.positions._total == 0 && jsonOb.cv.educations._total >= 1) {
-						return "http://localhost/results.json";
-					}
-					else if (jsonOb.cv.positions.values[0].title.indexOf("java") != -1){
-						return "http://localhost/results_software_engineer_java.json";
-					}
-					else {
-						return "http://localhost/results_software_engineer.json";
-					}
-				})(),*/
-				"url": window.hostname + "/api/search/"/*"http://localhost/mock.json"*//*apiURL + "search"*/,
+			window.console.log("searchCallback() :  " + json);
+
+            $.ajax({
+			    "url": window.hostname + "/api/search/",
 				"async": true,
 				"type": "post",
 				"data": {"content": json},
 				"success": function (data, textStatus, jqXHR) {
-					//window.console.log("Din ajax : " + JSON.stringify(data));
-					window.console.log("Asta e text: " + jqXHR.responseText);
 					if (typeof(data) == "string") {
 						pageAPI.livePreviewAPI.update(JSON.parse(data));
 					}
@@ -81,15 +68,13 @@ var app = (function() {
 				}
 			});
 		};
-		
-		
-		
-		
+
+
 		pageAPI.cvInputAPI.addSearchCallback(searchCallback);
 		searchCallback();
-		
+
 	};
-	
+
 
     var getCV = function(){
 		$.ajax({
